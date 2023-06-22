ce37dc09ce80e19d99d18806174aa1a5d825de00
Miriam Nava Mucino <mirichi@MacBook-Air-de-Miriam.local>
2023-06-22 15:00:58 -0600

    Added repositories buttons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,11 @@ import React from "react";
 const Projects = () => {
     return (
         <>
-            <a href="">Technical test 99minutos</a>
+            <button><a href="https://github.com/MiriamNM">GitHub Profile</a></button>
+            <button><a href="https://github.com/MiriamNM/99minutos-frontend-interview-test-2/tree/JR-miriamnava/space-land">Technical test 99minutos</a></button>
+            <button><a href="https://github.com/MiriamNM/Eight-queens-puzzle">Technical test Cuenca, Eight-queens-puzzle</a></button>
+            <button><a href="https://github.com/Jael91/CDMX010-burger-queen-api-client">CDMX010-burger-queen-api-client</a></button>
+            <button><a href="https://github.com/MiriamNM/CDMX010-md-links">CDMX010-md-links</a></button>
 
         </>   
     )
