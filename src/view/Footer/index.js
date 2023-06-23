196579a6a7a482a27b0d57df1241e934ab4b25bc
Miriam Nava Mucino <mirichi@MacBook-Air-de-Miriam.local>
2023-06-23 14:08:34 -0600

    Added first step to Send Email

--- src/view/Footer/index.js
+++ src/view/Footer/index.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from 'antd';
+import ModalEmail from '../../components/ModalEmail';
 
 const Footer = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+      };
+      const handleOk = () => {
+        setIsModalOpen(false);
+      };
+      const handleCancel = () => {
+        setIsModalOpen(false);
+      };
+
     return (
         <footer class="h-20 bg-pink-555 flex flex-col items-center font-semibold" style={{'color':'white'}}>
-            <h3>Contact with me</h3>
-            <p>Email: miriamnavamucino@gmail.com</p> 
+            <h3>Contact with me:</h3>
+            <Button type="text" block onClick={showModal} style={{'color':'white', 'font-family': 'Azeret Mono', 'font-weight': 'bold'}}>
+                Miriam's Email
+            </Button>
+            <ModalEmail 
+                isModalOpen={isModalOpen}
+                handleOk={handleOk}
+                handleCancel={handleCancel}
+            />
         </footer>   
     )
 };
