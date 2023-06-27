186d2d3967106b60614071b420ca72b3f8a4c7bd
Miriam Nava <71458522+MiriamNM@users.noreply.github.com>
2023-06-26 20:08:37 -0600

    The header was changed

--- src/view/Header/index.js
+++ src/view/Header/index.js
@@ -14,28 +14,51 @@ const Header = () => {
   // };
 
   return (
-    <div class="h-15 bg-pink-555 display: flex">
+    <nav class="bg-pink-555 border-gray-200 dark:bg-gray-900 flex">
       <div>
-        <img src={logo} alt="logo" class="w-1/2" />
+        <img src={logo} alt="logo" class="w-60" />
       </div>
-      <div class="flex">
-        {routes.map((route) => {
-          return (
-            <button class="mr-6 flex items-center font-semibold">
-              <NavLink
-                class="flex text-center sm:justify-center space-x-4"
-                to={route.to}
-                style={({ isActive }) => ({
-                  color: isActive ? "yellow" : "white",
-                })}
-              >
-                {route.text}
-              </NavLink>
-            </button>
-          );
-        })}
+      <div>
+        <ul class="font-medium flex flex-col md:flex-row md:space-x-10 md:mt-8 pl-10">
+          {routes.map((route) => {
+            return (
+              <li>
+                <NavLink
+                  to={route.to}
+                  style={({ isActive }) => ({
+                    color: isActive ? "yellow" : "white",
+                  })}
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  {route.text}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </div>
+    </nav>
+    // <nav class="h-15 bg-pink-555 flex">
+    //   <div>
+    //     <img src={logo} alt="logo" class="w-1/2" />
+    //   </div>
+    //   <div class="flex justify-around">
+    //     {routes.map((route) => {
+    //       return (
+    //         <button class="font-semibold">
+    //           <NavLink
+    //             to={route.to}
+    //             style={({ isActive }) => ({
+    //               color: isActive ? "yellow" : "white",
+    //             })}
+    //           >
+    //             {route.text}
+    //           </NavLink>
+    //         </button>
+    //       );
+    //     })}
+    //   </div>
+    // </nav>
   );
 };
 
