548b08291047bdea4832b83106b1c8d866a6978b
Miriam Nava <71458522+MiriamNM@users.noreply.github.com>
2023-07-05 22:24:52 -0600

    the header buttons was changed

--- src/view/Header/index.js
+++ src/view/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Button } from "antd";
 import logo from "../../assets/logo.png";
 
 const Header = () => {
@@ -13,15 +14,19 @@ const Header = () => {
           {routes.map((route) => {
             return (
               <li>
-                <NavLink
-                  to={route.to}
-                  style={({ isActive }) => ({
-                    color: isActive ? "yellow" : "white",
-                  })}
+                <Button
+                  type="primary"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  {route.text}
-                </NavLink>
+                  <NavLink
+                    to={route.to}
+                    style={({ isActive }) => ({
+                      color: isActive ? "yellow" : "white",
+                    })}
+                  >
+                    {route.text}
+                  </NavLink>
+                </Button>
               </li>
             );
           })}
