94f5ec40c8156f71ca2d4ad4ae2254ef63a14ae6
Miriam Nava <71458522+MiriamNM@users.noreply.github.com>
2023-08-03 16:34:07 -0600

    inprovements mobile version and dashboard version

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ import TextAboutMe from "./TextAboutMe";
 
 const AboutMe = () => {
   return (
-    <div className="px-10 py-10 my-20 sm:px-10 sm:py-2 md:px-10 md:py-2 text-dark">
+    <div className="px-20 py-10 mx-20 sm:px-1 sm:py-2 md:px-1 md:py-2 text-dark">
       <TextAboutMe />
       <ButtonsAboutMe />
     </div>
